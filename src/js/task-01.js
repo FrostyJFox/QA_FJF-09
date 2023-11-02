404b17c6b771f06c54f3d8059898a5f2be0056e0
FrostyJFox <132783113+FrostyJFox@users.noreply.github.com>
2023-11-02 18:14:06 +0100

    change task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,12 @@
 // Get the ul#categories element
 const categoriesList = document.getElementById('categories');
 
+
 // Find all the li.item elements within ul#categories
 const categoryItems = categoriesList.querySelectorAll('li.item');
 
 // Count and display the number of categories
-console.log(`Number of categories: ${categoryItems.length}`);
+console.log("Number of categories:", categoryItems.length);
 
 // Iterate through each li.item element and display category title and number of elements
 categoryItems.forEach((categoryItem) => {
@@ -16,7 +17,7 @@ categoryItems.forEach((categoryItem) => {
   const categoryElements = categoryItem.querySelectorAll('ul li');
 
   // Count and display the number of elements within the current category
-  console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements: ${categoryElements.length}`);
+  console.log("Category:", categoryTitle);
+  console.log("Elements:", categoryElements.length);
 });
 
