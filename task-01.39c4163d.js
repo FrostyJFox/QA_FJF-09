7af53dbb7d6bbbcaf800cab48a81dd245dce6871
FrostyJFox <132783113+FrostyJFox@users.noreply.github.com>
2023-11-02 18:14:18 +0000

    Deploying to gh-pages from @ FrostyJFox/QA_FJF-09@4562a3f27ed066a1ed24f0306cc453f92e33c5ce 🚀

---
+++ task-01.39c4163d.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("categories").querySelectorAll("li.item");console.log("Number of categories:",e.length),e.forEach((e=>{const o=e.querySelector("h2").textContent,l=e.querySelectorAll("ul li");console.log("Category:",o),console.log("Elements:",l.length)}));
+//# sourceMappingURL=task-01.39c4163d.js.map
